d49532831360a96e7bff969062d8e917f43c59d2
Daniel Colascione <dancol@dancol.org>
2011-12-29 10:00:08 -0800

    Remove outdated politics

--- lisp/terminal.el
+++ lisp/terminal.el
@@ -24,15 +24,6 @@
 
 ;;; Commentary:
 
-;;; This file has been censored by the Communications Decency Act.
-;;; That law was passed under the guise of a ban on pornography, but
-;;; it bans far more than that.  This file did not contain pornography,
-;;; but it was censored nonetheless.
-
-;;; For information on US government censorship of the Internet, and
-;;; what you can do to bring back freedom of the press, see the web
-;;; site http://www.vtw.org/
-
 ;;; Code:
 
 ;;>>TODO
